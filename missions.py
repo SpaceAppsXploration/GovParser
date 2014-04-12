09e68e19c5a94f82145f2fe08ea10933b8b3c7b7
Lorenzo <lorenzo@linux-74kc.site>
2014-04-12 15:45:15 +0200

    implemented urllib and BeautifulSoup

--- missions.py
+++ missions.py
@@ -1,9 +1,17 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+def is_link(tag):
+	if tag.name == 'a':
+	    print(tag)
+
+# define URL
+
 url = 'http://solarsystem.nasa.gov/missions/profile.cfm?'
 
 target = 'Target=Mars'
+
+# get page
 
 reponse = urllib.request.urlopen(str(url+target))
 
@@ -11,6 +19,14 @@ htmlDoc = reponse.read()
 
 soup = BeautifulSoup(htmlDoc)
 
-hyperlinks = soup.find_all('a')
+# find all hyperlinks
 
-print(hyperlinks)
+parent = soup.find_all("div", class_="l2text")
+
+parent = BeautifulSoup(''.join(parent))
+
+hyperlinks = parent.find_all(is_link)
+
+# get links inside div class l2text
+
+#print(hyperlinks)
